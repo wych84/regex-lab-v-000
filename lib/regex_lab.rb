95f206cbdc65e22dd1d2fb641bf379579fcb1b74
Joyce Chun <wooyangee@gmail.com>
2018-04-16 18:19:48 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -20,7 +20,7 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-    if phone.scan(/\d+.\d+.\d+/)
+    if phone.match(/\d+.\d+.\d+/)
       true
     else
       false
