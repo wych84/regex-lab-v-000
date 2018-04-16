1334a00433fb3030f47f4d03875bc7f0925c6a97
Joyce Chun <wooyangee@gmail.com>
2018-04-16 17:23:10 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,25 +1,24 @@
 def starts_with_a_vowel?(word)
-  word.scan(/\A[aeiou]/i)
-end 
-    # return true
-#   elsif !word.match(/\A[aeiou]/i)
-#     return false
-#   end
-# end
+  if word.scan(/\A[aeiou]/i)
+    return true
+  elsif !word.match(/\A[aeiou]/i)
+    return false
+  end
+end
 
 
-# def words_starting_with_un_and_ending_with_ing(text)
-#     text.scan(/\b[un]\w+[ing]\b/)
-# end
+def words_starting_with_un_and_ending_with_ing(text)
+    text.scan(/\b[un]\w+[ing]\b/)
+end
 
-# def words_five_letters_long(text)
-#     text.scan(/\b\w{5}\b/)
-# end
+def words_five_letters_long(text)
+    text.scan(/\b\w{5}\b/)
+end
 
-# def first_word_capitalized_and_ends_with_punctuation?(text)
-#     text.match()
-# end
+def first_word_capitalized_and_ends_with_punctuation?(text)
+    text.match()
+end
 
-# def valid_phone_number?(phone)
+def valid_phone_number?(phone)
 
-# end
+end
