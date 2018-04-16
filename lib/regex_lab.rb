c13831c131359f8e8bd804fe606b015109b4af8b
Joyce Chun <wooyangee@gmail.com>
2018-04-16 17:20:51 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,9 +15,9 @@ def words_five_letters_long(text)
     text.scan(/\b\w{5}\b/)
 end
 
-# def first_word_capitalized_and_ends_with_punctuation?(text)
-
-# end
+def first_word_capitalized_and_ends_with_punctuation?(text)
+    text.match()
+end
 
 # def valid_phone_number?(phone)
 
