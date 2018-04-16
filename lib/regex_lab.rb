0e8733822b92e0ef1b08eeb77a76dd149c86bf17
Joyce Chun <wooyangee@gmail.com>
2018-04-16 17:05:32 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,8 @@
 def starts_with_a_vowel?(word)
-
+  if word.match(\/A[aeiou]/i)
+    return true
+  elsif !word.match(\/A[aeiou]/i)
+    return false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
