86e269c002a95019558674ec2ea2d95e41a47d78
Joyce Chun <wooyangee@gmail.com>
2018-04-16 17:23:31 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,5 @@
 def starts_with_a_vowel?(word)
-  if word.scan(/\A[aeiou]/i)
+  if word.match(/\A[aeiou]/i)
     return true
   elsif !word.match(/\A[aeiou]/i)
     return false
@@ -16,7 +16,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-    text.match()
+    if text.match()
 end
 
 def valid_phone_number?(phone)
