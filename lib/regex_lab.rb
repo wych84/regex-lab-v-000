8b0ed06c7953c16a2a69f1feba32ec93d2b1ee9c
Joyce Chun <wooyangee@gmail.com>
2018-04-16 18:06:56 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -20,7 +20,7 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-    if phone.scan(/\d{10}|[(\d{3})\d{3}\-\d{4}]|[\d{3}\s\d{3}\s\d{4}]|[(\d{3})\d{7}]/)
+    if phone.scan(//)
       true
     else
       false
