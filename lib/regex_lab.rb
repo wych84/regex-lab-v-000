f33dfb299bc69a14f9b1f80721a6e162caa0e664
Joyce Chun <wooyangee@gmail.com>
2018-04-16 18:02:24 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -21,4 +21,5 @@ end
 
 def valid_phone_number?(phone)
     !!(phone.scan(/\d{10}|[(\d{3})\d{3}\-\d{4}]|[\d{3}\s\d{3}\s\d{4}]|[(\d{3})\d{7}]/))
+    !(phone.scan(/\d{10}|[(\d{3})\d{3}\-\d{4}]|[\d{3}\s\d{3}\s\d{4}]|[(\d{3})\d{7}]/))
 end
