aa9fa430688b88ac84b8c62033cf7e2bd784c65d
Joyce Chun <wooyangee@gmail.com>
2018-04-16 17:11:06 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -7,9 +7,9 @@ def starts_with_a_vowel?(word)
 end
 
 
-# def words_starting_with_un_and_ending_with_ing(text)
+def words_starting_with_un_and_ending_with_ing(text)
 
-# end
+end
 
 # def words_five_letters_long(text)
 
