403b908918bc65e03c8a1b87c9517e54cc428f40
Joyce Chun <wooyangee@gmail.com>
2018-04-16 18:21:59 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,28 @@
 def starts_with_a_vowel?(word)
-
+  if word.match(/\A[aeiou]/i)
+    return true
+  elsif !word.match(/\A[aeiou]/i)
+    return false
+  end
 end
 
-def words_starting_with_un_and_ending_with_ing(text)
 
+def words_starting_with_un_and_ending_with_ing(text)
+    text.scan(/\b[un]\w+[ing]\b/)
 end
 
 def words_five_letters_long(text)
-
+    text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+    !!(text.match(/^[A-Z].+[[:punct:]]$/))
 end
 
 def valid_phone_number?(phone)
-
+    if phone.match(/\d+.\d+.\d+/)
+      true
+    else
+      false
+    end
 end
