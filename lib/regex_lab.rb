dfb3ea048feaeecf278abddfd17108dae01cf5be
Joyce Chun <wooyangee@gmail.com>
2018-04-16 17:38:26 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -16,9 +16,9 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-   !!(text.match(/^[A-Z].+[[:punct:]]$/))
+    !!(text.match(/^[A-Z].+[[:punct:]]$/))
 end
 
 def valid_phone_number?(phone)
-
+    !!(text.scan(//))
 end
