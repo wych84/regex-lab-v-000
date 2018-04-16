62755d614131f310af54dd7c8ed6af9996cf0012
Joyce Chun <wooyangee@gmail.com>
2018-04-16 18:01:25 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -20,5 +20,5 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-    !! phone.scan(
+    !! (phone.scan(\d{10}|[(\d{3})\d{3}\-\d{4}]|[\d{3}\s\d{3}\s\d{4}]|[(\d{3})\d{7}]))
 end
